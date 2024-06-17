07cebec9a979d45557ea157c8e9161edf46717aa
maxH4k5s <123451195+maxH4k5s@users.noreply.github.com>
2024-06-18 01:06:24 +0530

    codepan-clone

---
+++ src/components/Code.jsx
@@ -0,0 +1,14 @@
+
+import Editor from "./Editor";
+
+const Code = () => {
+    return (
+        <>
+            <Editor heading="Html" />
+            <Editor heading="Css" />
+            <Editor heading="Javascript" />
+        </>
+    )
+}
+
+export default Code;
