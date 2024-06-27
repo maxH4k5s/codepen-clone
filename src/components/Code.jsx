d39ee3dcd2d2460defb1dce355108b45e80b77fb
maxH4k5s <123451195+maxH4k5s@users.noreply.github.com>
2024-06-27 14:40:23 +0530

    Codepen Clone

--- src/components/Code.jsx
+++ src/components/Code.jsx
@@ -1,13 +1,49 @@
-
+import { useContext } from "react";
 import Editor from "./Editor";
+import { Box, styled } from '@mui/material';
+import { DataContext } from '../context/DataProvider';
+
+
+const Container = styled(Box)`
+    display: flex;
+    background-color: #060606;
+    height: 55vh;
+`
 
 const Code = () => {
+
+    const { html, setHtml, css, setCss, js, setJs } = useContext(DataContext)
+
     return (
-        <>
-            <Editor heading="Html" />
-            <Editor heading="Css" />
-            <Editor heading="Javascript" />
-        </>
+        <Container>
+            <Editor
+                language="xml"
+                heading="Html"
+                icon="/"
+                color="#ff3c41"
+                value={html}
+                onChange={setHtml}
+            />
+
+            <Editor
+                language="css"
+                heading="Css"
+                icon="*"
+                color="#0ebeff"
+                value={css}
+                onChange={setCss}
+
+            />
+            <Editor
+                language="javascript"
+                heading="Javascript"
+                icon="( )"
+                color="#fcd000"
+                value={js}
+                onChange={setJs}
+
+            />
+        </Container>
     )
 }
 
--- src/components/Editor.jsx
+++ src/components/Editor.jsx
@@ -1,6 +1,7 @@
 import { Box, styled } from '@mui/material';
 import CloseFullscreenIcon from '@mui/icons-material/CloseFullscreen';
 
+import { useState } from 'react'
 import { Controlled as ControlledEditor } from 'react-codemirror2';
 
 import 'codemirror/lib/codemirror.css';
@@ -11,6 +12,14 @@ import 'codemirror/mode/css/css';
 
 
 import '../App.css'
+
+const Container = styled(Box)`
+    flex-grow: 1;
+    flex-basis: 0;
+    display: flex;
+    flex-direction: column;
+    padding: 0 8px 8px;
+`
 
 const Heading = styled(Box)`
     background: #1d1e22;
@@ -26,16 +35,21 @@ const Header = styled(Box)`
     font-weight: 700;
 `;
 
-const Editor = ({ heading }) => {
+const Editor = ({ heading, language, icon, color, value, onChange }) => {
 
+    const [open, setOpen] = useState(true);
+
+    const handleChange = (editor, data, value) => {
+        onChange(value);
+    }
     return (
-        <Box>
+        <Container style={open ? null : { flexGrow: 0 }}>
             <Header>
                 <Heading>
                     <Box
                         component="span"
                         style={{
-                            background: 'red',
+                            background: color,
                             height: 20,
                             width: 20,
                             display: 'flex',
@@ -43,20 +57,31 @@ const Editor = ({ heading }) => {
                             borderRadius: 5,
                             marginRight: 5,
                             paddingBottom: 2,
+                            color: '#000'
                         }}
-                    >/</Box>
+                    >{icon}</Box>
                     {heading}
                 </Heading>
-                <CloseFullscreenIcon />
+                <CloseFullscreenIcon
+                    fontSize='small'
+                    style={{ alignSelf: 'center' }}
+                    onClick={() => setOpen(prevState => !prevState)}
+
+                />
             </Header>
             <ControlledEditor
                 className='controlled-editor'
+                value={value}
+                onBeforeChange={handleChange}
                 options={{
+                    lineWrapping: true,
+                    lint: true,
+                    mode: language,
                     theme: 'material',
-                    lineNumbers: true,
+                    lineNumbers: true
                 }}
             />
-        </Box>
+        </Container>
     )
 }
 
